6cf7148d1fda25b1ddf6f017aeabfbe4c37acc73
Cody Roberts <pithlyx@gmail.com>
2023-07-08 19:42:36 -0600

    pip library setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='poissonate',
+    version='1.0',
+    license='MIT',
+    author="Cody Roberts",
+    author_email='pithlyx@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/gmyrianthous/example-publish-pypi',
+    keywords='poisson disc sampling',
+    install_requires=[
+        'numpy',
+        'scipy',
+    ],
+
+)
